f620faa8398e66d7a1c326d5909d4e16d70a8d53
FGuerreir0 <ffsguerreiro@hotmail.com>
2025-02-01 13:06:53 +0000

    -

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 Setup on:
 - Heroku (https://dashboard.heroku.com/apps/my-twitch-bot)
+
+
+# ToDo List:
+- Fix engaging following message
+- implemet !so feature
+- Loyalty points to spend in funny stuff - save points in a json
+- points leaderboard 
+- text-to-speech ?? - npm - say using points
+- VIP Lottery
+- schedule command 
+- rock paper scissors game
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //https://twitchtokengenerator.com/
 
 const oneLinerJoke = require('one-liner-joke');
-const { translate } = require('bing-translate-api');
 const fn = require('./util/fn.js');
 require('dotenv').config();
 const tmi = require('tmi.js');
@@ -24,32 +23,44 @@ let todayModArray = [];
 
 client.connect();
 
+const modMessages = {
+  'thehasputimm': `Cuidado o @{mod} está no chat! Ainda joga LOL e é rank Bronze 2! NotLikeThis`,
+  'tioeman': `O melhor camionista pimba da Twitch @{mod} chegou! Vamos meter a cassete do Quim Barreiros para ele dançar! DinoDance`,
+  'adanielapereira': `Cuidado a @{mod} está a praticar para dominar o mundo com o SIMS 4, até tem todas as expansões! EarthDay PowerUpR`
+};
+
 function firstModMesg(modInfo, channel) {
   todayModArray.push(modInfo.username);
-  switch(modInfo.username.toLowerCase()) {
-    case 'thehasputimm':
-      client.say(channel, `Cuidado o @${modInfo.username} está no chat! Ainda joga LOL e é rank Silver 2! NotLikeThis `);
-      break;
-    case 'tioeman':
-      client.say(channel, `O melhor camonionista pimba da Twitch @${modInfo.username} chegou! Vamos meter a cassete do Quim Barreiros para ele dançar! DinoDance`);
-      break;
-    case 'adanielapereira':
-      client.say(channel, `Cuidado a @${modInfo.username} está a praticar para dominar o mundo com o SIMS 4, até tem todas as expansões! EarthDay PowerUpR`)
-    default:
-      break;
+  let message = modMessages[modInfo.username.toLowerCase()];
+  if (message) {
+    client.say(channel, message.replace('{mod}', modInfo.username));
   }
 }
 
 client.on('connected', async () => {
   setInterval(()=>{
     client.say('#fguerreir0', `Olá malta, obrigado por estarem a assistir aproveitem e façam follow e !comandos para mais informações! HSCheers`)
-  }, 30 * 60 * 1000)
+  }, 60 * 60 * 1000)
 })
 
-
 client.on('message', async (channel, tags, message, self) => {
+  const commands = {
+    'ola': (channel, tags) => client.say(channel, `Olá @${tags.username}! Bem-vindo, junta-te a nós e bebe um copo! HSCheers`),
+    'specs': (channel) => client.say(channel, `Processador: AMD Ryzen 7 3700x | Memória Ram: 64GB | Placa Gráfica: NVIDIA GeForce RTX 3060 | MotherBoard: B450 Aorus Elite V2 | Fonte: Azza PSAZ-650W`),
+    'website': (channel) => client.say(channel, `https://fabioguerreiro.pt`),
+    'social': (channel) => client.say(channel, `Instagram: https://www.instagram.com/fguerreir0 | LinkedIn: https://www.linkedin.com/in/fabiofsguerreiro | GitHub: https://github.com/FGuerreir0`),
+    'comandos': (channel) => client.say(channel, `!ola, !specs, !social, !website, !piada`),
+    'piada': async (channel, tags) => {
+      let joke = fn.getJoke(oneLinerJoke);
+      if (joke) {
+        let jokeToSend = joke.includes('(NAME)') ? joke.replace('(NAME)', tags.username) : joke;
+        client.say(channel, jokeToSend);
+      }
+    }
+  };
+  
   let currentdate = fn.getcurrentdate(); 
-  console.log(channel)
+  
   if  (defaultdate != currentdate && tags.mod && !todayModArray.find(element => element === tags.username)) {
       firstModMesg(tags, channel)
   }
@@ -59,54 +70,11 @@ client.on('message', async (channel, tags, message, self) => {
     client.say(channel, `Chat paguem uma cerveja ao @${tags.username}! Primeira mensagem na stream! Bem-vindo HSCheers HSCheers`);
   }
 
-  // Ignore echoed messages.
 	if(self || !message.startsWith('!')) return;
-
 	const args = message.slice(1).split(' ');
 	const command = args.shift().toLowerCase();
 
-  switch(command.toLowerCase()) {
-    case 'hello':
-    case 'hey':
-    case 'ola':
-      client.say(channel, `Olá @${tags.username}! Bem-vindo, junta-te a nós e bebe um copo! HSCheers`);
-      break;
-    case 'specs':
-      client.say(channel, `Processador: AMD Ryzen 7 3700x | Memória Ram: 64GB | Placa Gráfica: NVIDIA GeForce RTX 3060 | MotherBoard: B450 Aorus Elite V2 | Fonte: Azza PSAZ-650W`);
-      break;
-    case 'website':
-      client.say(channel, `https://fabioguerreiro.pt`);
-      break;
-    case 'social':
-      client.say(channel, `Instagram: https://www.instagram.com/fguerreir0 | LinkedIn: https://www.linkedin.com/in/fabiofsguerreiro | GitHub: https://github.com/FGuerreir0`);
-      break;
-    case 'comandos':
-      client.say(channel, `!ola, !specs, !social, !website, !piada, !rank`);
-      break;
-    case 'rank':
-      client.say(channel, `O @${tags.username} é rank Veterano 3 no Pokemon Unite! NotLikeThis`);
-      break;
-    case 'piada':
-      let joke = fn.getJoke(oneLinerJoke, translate);
-      if(joke) {
-        //translate(joke, null, 'pt').then(res => {
-          //let translatedJoke = res.translation;
-          let translatedJoke = joke;
-          if(translatedJoke.includes('(NAME)')){
-            translatedJoke = translatedJoke.replace('(NAME)', tags.username);
-          }
-          client.say(channel, translatedJoke);
-        //}).catch(err => {
-          //console.error(err);
-          //reject(err);
-        //});
-        
-      }
-      //case 'pokemon':
-      // !pokemon charizard
-      //client.say(channel, `https://pokeapi.co/api/v2/pokemon/${args[0].toLowerCase()}`);
-
-    default:
-      break;
+  if (commands[command]) {
+    commands[command](channel, tags);
   }
 });
